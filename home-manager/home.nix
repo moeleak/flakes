582a3db93e0bb566ea76eee66ba850f22418ab62
MoeLeak <i@leak.moe>
2026-01-06 15:09:34 +0800

    refactor: home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,239 +19,200 @@ in
     inputs.plasma-manager.homeModules.plasma-manager
   ];
 
-  config = lib.mkMerge [
-    (lib.optionalAttrs (isLinux && host == "LoliIsland-PC-Nix") {
-      programs.plasma = {
-        enable = true;
-        shortcuts = {
-          "services/org.kde.krunner.desktop"._launch = [ "Meta+Space" ];
-          "services/org.kde.spectacle.desktop"._launch = [ "Alt+@" ];
-          "services/org.kde.spectacle.desktop".FullScreenScreenShot = [ "Alt+!" ];
-          "services/org.kde.spectacle.desktop".ActiveWindowScreenShot = [ "Alt+#" ];
-        };
+  home.username = if isLinux then "moeleak" else "lolimaster";
+  home.homeDirectory = if isLinux then "/home/moeleak" else "/Users/lolimaster";
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  home.packages =
+    [
+      inputs.moevim.packages.${pkgs.stdenv.hostPlatform.system}.default
+      pkgs.fastfetch
+      pkgs.yazi
+      pkgs.devenv
+      pkgs.direnv
+      pkgs.codex
+
+      pkgs.clang-tools
+      pkgs.clang
+      pkgs.llvm
+      pkgs.cmake
+      pkgs.ccls
+      pkgs.gnumake
+
+      pkgs.zip
+      pkgs.xz
+      pkgs.unzip
+      pkgs.p7zip
+
+      pkgs.iperf3
+      pkgs.mtr
+      pkgs.aria2
+      pkgs.nmap
+
+      pkgs.file
+      pkgs.which
+      pkgs.tree
+
+      pkgs.ripgrep
+      pkgs.jq
+      pkgs.yq-go
+      pkgs.eza
+      pkgs.fzf
+      pkgs.tmux
+      pkgs.btop
+      pkgs.gitmux
+    ]
+    ++ (lib.optionals isLinux [
+      pkgs.ghostty
+      pkgs.libreoffice
+      pkgs._64gram
+      pkgs.audacious
+      pkgs.moonlight-qt
+      pkgs.hmcl
+      pkgs.wl-clipboard
+
+      pkgs.dnsutils
+      pkgs.ldns
+      pkgs.socat
+      pkgs.ipcalc
+
+      pkgs.strace
+      pkgs.ltrace
+      pkgs.lsof
+      pkgs.iotop
+      pkgs.iftop
+      pkgs.sysstat
+      pkgs.lm_sensors
+      pkgs.ethtool
+      pkgs.pciutils
+      pkgs.usbutils
+
+      pkgs.nix-output-monitor
+      pkgs.glow
+
+      pkgs.cowsay
+      pkgs.gnused
+      pkgs.gnutar
+      pkgs.gawk
+      pkgs.zstd
+      pkgs.gnupg
+    ]);
+
+  programs.zen-browser.enable = isLinux;
+
+  programs.plasma = lib.mkIf isLinux {
+    enable = (host == "LoliIsland-PC-Nix" || host == "LoliIsland-Laptop-Nix");
+    shortcuts =
+      {
+        "services/org.kde.spectacle.desktop"._launch = [ "Alt+@" ];
+        "services/org.kde.spectacle.desktop".FullScreenScreenShot = [ "Alt+!" ];
+        "services/org.kde.spectacle.desktop".ActiveWindowScreenShot = [ "Alt+#" ];
+      }
+      // (lib.optionalAttrs (host == "LoliIsland-PC-Nix") {
+        "services/org.kde.krunner.desktop"._launch = [ "Meta+Space" ];
+      });
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    ignores = [
+      ".cache/"
+      ".DS_Store"
+      ".idea/"
+      "*.swp"
+      "built-in-stubs.jar"
+      "dumb.rdb"
+      ".elixir_ls/"
+      ".vscode/"
+      "npm-debug.log"
+      "shell.nix"
+      ".direnv"
+    ];
+    settings = {
+      user.name = "MoeLeak";
+      user.email = "i@leak.moe";
+      alias = {
+        st = "status";
+        lg = "log --oneline --graph --all --decorate";
       };
-    })
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+    };
+  };
+  programs.fish = {
+    enable = true;
 
-    (lib.optionalAttrs (isLinux && host == "LoliIsland-Laptop-Nix") {
-      programs.plasma = {
-        enable = true;
-        shortcuts = {
-          "services/org.kde.spectacle.desktop"._launch = [ "Alt+@" ];
-          "services/org.kde.spectacle.desktop".FullScreenScreenShot = [ "Alt+!" ];
-          "services/org.kde.spectacle.desktop".ActiveWindowScreenShot = [ "Alt+#" ];
-        };
-      };
-    })
+    shellInit = lib.mkIf isDarwin ''
+      source /Users/lolimaster/miniconda3/etc/fish/conf.d/conda.fish
+      for p in /run/current-system/sw/bin /etc/profiles/per-user/$USER/bin
+        if not contains $p $fish_user_paths
+          set -g fish_user_paths $p $fish_user_paths
+        end
+      end
+    '';
 
-    (lib.mkIf isLinux {
-      home.username = "moeleak";
-      home.homeDirectory = "/home/moeleak";
-      programs.zen-browser.enable = true;
-      home.packages = [
-        pkgs.ghostty
-        pkgs.libreoffice
-        pkgs._64gram
-        pkgs.audacious
-        pkgs.moonlight-qt
-        pkgs.hmcl
-        pkgs.wl-clipboard
+    interactiveShellInit = ''
+      set -g fish_greeting ""
 
-        # networking tools
-        pkgs.mtr
-        pkgs.iperf3
-        pkgs.dnsutils
-        pkgs.ldns
-        pkgs.aria2
-        pkgs.socat
-        pkgs.nmap
-        pkgs.ipcalc
+      set -g fish_color_autosuggestion brblack
+      set -g fish_color_cancel --reverse
+      set -g fish_color_command 87afd7
+      set -g fish_color_comment red
+      set -g fish_color_cwd green
+      set -g fish_color_cwd_root red
+      set -g fish_color_end green
+      set -g fish_color_error brred
+      set -g fish_color_escape brcyan
+      set -g fish_color_history_current --bold
+      set -g fish_color_host normal
+      set -g fish_color_host_remote yellow
+      set -g fish_color_keyword normal
+      set -g fish_color_match --background=brblue
+      set -g fish_color_normal normal
+      set -g fish_color_operator brcyan
+      set -g fish_color_option cyan
+      set -g fish_color_param cyan
+      set -g fish_color_quote yellow
+      set -g fish_color_redirection cyan --bold
+      set -g fish_color_search_match bryellow --background=brblack
+      set -g fish_color_selection white --bold --background=brblack
+      set -g fish_color_status red
+      set -g fish_color_user brgreen
+      set -g fish_color_valid_path --underline
 
-        # misc
-        pkgs.cowsay
-        pkgs.file
-        pkgs.which
-        pkgs.tree
-        pkgs.gnused
-        pkgs.gnutar
-        pkgs.gawk
-        pkgs.zstd
-        pkgs.gnupg
+      set -g fish_pager_color_background normal
+      set -g fish_pager_color_completion normal
+      set -g fish_pager_color_description yellow --italics
+      set -g fish_pager_color_prefix normal --bold --underline
+      set -g fish_pager_color_progress brwhite --background=cyan
+      set -g fish_pager_color_secondary_background normal
+      set -g fish_pager_color_secondary_completion normal
+      set -g fish_pager_color_secondary_description normal
+      set -g fish_pager_color_secondary_prefix normal
+      set -g fish_pager_color_selected_background --reverse
+      set -g fish_pager_color_selected_completion normal
+      set -g fish_pager_color_selected_description normal
+      set -g fish_pager_color_selected_prefix normal
 
-        # nix related
-        pkgs.nix-output-monitor
+      set -g fish_key_bindings fish_default_key_bindings
+    '';
+  };
 
-        # productivity
-        pkgs.glow
+  programs.starship.enable = true;
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
 
-        pkgs.iotop
-        pkgs.iftop
+  xdg.configFile."ghostty/config".text = ''
+    keybind = ctrl+t=new_tab
+    cursor-style = bar
+    cursor-style-blink = false
+    shell-integration = none
+  '';
 
-        # system call monitoring
-        pkgs.strace
-        pkgs.ltrace
-        pkgs.lsof
-
-        # system tools
-        pkgs.sysstat
-        pkgs.lm_sensors
-        pkgs.ethtool
-        pkgs.pciutils
-        pkgs.usbutils
-      ];
-    })
-
-    (lib.mkIf isDarwin {
-      home.username = "lolimaster";
-      home.homeDirectory = "/Users/lolimaster";
-      programs = {
-        fish.enable = true;
-        fish.shellInit = ''
-          source /Users/lolimaster/miniconda3/etc/fish/conf.d/conda.fish
-          for p in /run/current-system/sw/bin /etc/profiles/per-user/$USER/bin
-            if not contains $p $fish_user_paths
-              set -g fish_user_paths $p $fish_user_paths
-            end
-          end
-
-        '';
-      };
-
-    })
-
-    {
-      home.sessionVariables = {
-        EDITOR = "nvim";
-      };
-
-      xdg.configFile."ghostty/config".text = ''
-        keybind = ctrl+t=new_tab
-        cursor-style = bar
-        cursor-style-blink = false
-        shell-integration = none
-      '';
-
-      home.packages = [
-        inputs.moevim.packages.${pkgs.stdenv.hostPlatform.system}.default
-
-        pkgs.fastfetch
-
-        pkgs.yazi
-        pkgs.devenv
-        pkgs.direnv
-
-        pkgs.codex
-
-        # cplusplus coding
-        pkgs.clang-tools
-        pkgs.clang
-        pkgs.llvm
-        pkgs.cmake
-        pkgs.ccls
-        pkgs.gnumake
-
-        # chat
-        #pkgs.wechat
-
-        # archives
-        pkgs.zip
-        pkgs.xz
-        pkgs.unzip
-        pkgs.p7zip
-
-        # utils
-        pkgs.ripgrep
-        pkgs.jq
-        pkgs.yq-go
-        pkgs.eza
-        pkgs.fzf
-        pkgs.tmux
-        pkgs.btop
-        pkgs.gitmux
-      ];
-
-      programs.direnv.enable = true;
-
-      programs.git = {
-        enable = true;
-        lfs.enable = true;
-        ignores = [
-          ".cache/"
-          ".DS_Store"
-          ".idea/"
-          "*.swp"
-          "built-in-stubs.jar"
-          "dumb.rdb"
-          ".elixir_ls/"
-          ".vscode/"
-          "npm-debug.log"
-          "shell.nix"
-          ".direnv"
-        ];
-        settings = {
-          user.name = "MoeLeak";
-          user.email = "i@leak.moe";
-          alias = {
-            st = "status";
-            lg = "log --oneline --graph --all --decorate";
-          };
-          init.defaultBranch = "main";
-          push.autoSetupRemote = true;
-          pull.rebase = true;
-        };
-      };
-
-      programs.fish = {
-        enable = true;
-        interactiveShellInit = ''
-          set fish_greeting
-          set -g fish_color_autosuggestion brblack
-          set -g fish_color_cancel --reverse
-          set -g fish_color_command 87afd7
-          set -g fish_color_comment red
-          set -g fish_color_cwd green
-          set -g fish_color_cwd_root red
-          set -g fish_color_end green
-          set -g fish_color_error brred
-          set -g fish_color_escape brcyan
-          set -g fish_color_history_current --bold
-          set -g fish_color_host normal
-          set -g fish_color_host_remote yellow
-          set -g fish_color_keyword normal
-          set -g fish_color_match --background=brblue
-          set -g fish_color_normal normal
-          set -g fish_color_operator brcyan
-          set -g fish_color_option cyan
-          set -g fish_color_param cyan
-          set -g fish_color_quote yellow
-          set -g fish_color_redirection cyan --bold
-          set -g fish_color_search_match bryellow --background=brblack
-          set -g fish_color_selection white --bold --background=brblack
-          set -g fish_color_status red
-          set -g fish_color_user brgreen
-          set -g fish_color_valid_path --underline
-
-          set -g fish_pager_color_background normal
-          set -g fish_pager_color_completion normal
-          set -g fish_pager_color_description yellow --italics
-          set -g fish_pager_color_prefix normal --bold --underline
-          set -g fish_pager_color_progress brwhite --background=cyan
-          set -g fish_pager_color_secondary_background normal
-          set -g fish_pager_color_secondary_completion normal
-          set -g fish_pager_color_secondary_description normal
-          set -g fish_pager_color_secondary_prefix normal
-          set -g fish_pager_color_selected_background --reverse
-          set -g fish_pager_color_selected_completion normal
-          set -g fish_pager_color_selected_description normal
-          set -g fish_pager_color_selected_prefix normal
-
-          set -g fish_greeting ""
-          set -g fish_key_bindings fish_default_key_bindings
-        '';
-
-      };
-      programs.starship.enable = true;
-
-      home.stateVersion = "25.11";
-    }
-  ];
+  home.stateVersion = "25.11";
 }

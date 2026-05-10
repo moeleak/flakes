c75755ab1c690bc1d07558ca67809bced05f27cb
MoeLeak <i@leak.moe>
2026-05-10 15:51:09 +0800

    sway: battery bar and volume control

--- desktop/sway.nix
+++ desktop/sway.nix
@@ -8,6 +8,42 @@
 let
   fcitx5Enabled =
     config.i18n.inputMethod.enable && config.i18n.inputMethod.type == "fcitx5";
+  pipewireEnabled = config.services.pipewire.enable;
+  swayStatus = pkgs.writeShellScript "sway-status" ''
+    battery_status() {
+      for capacity in /sys/class/power_supply/BAT*/capacity; do
+        [ -e "$capacity" ] || continue
+
+        dir=''${capacity%/capacity}
+        percent=$(${pkgs.coreutils}/bin/cat "$capacity" 2>/dev/null || true)
+        status=$(${pkgs.coreutils}/bin/cat "$dir/status" 2>/dev/null || true)
+
+        [ -n "$percent" ] || continue
+
+        case "$status" in
+          Charging) printf 'BAT %s%%+' "$percent" ;;
+          *) printf 'BAT %s%%' "$percent" ;;
+        esac
+        return 0
+      done
+
+      return 1
+    }
+
+    while :; do
+      if battery=$(battery_status); then
+        printf '%s | %s\n' "$battery" "$(${pkgs.coreutils}/bin/date +'%Y-%m-%d %H:%M')"
+      else
+        ${pkgs.coreutils}/bin/date +'%Y-%m-%d %H:%M'
+      fi
+      ${pkgs.coreutils}/bin/sleep 30
+    done
+  '';
+  swayConfig = pkgs.runCommand "sway-config" { } ''
+    substitute ${config.programs.sway.package}/etc/sway/config $out \
+      --replace-fail "status_command while date +'%Y-%m-%d %X'; do sleep 1; done" \
+                     "status_command ${swayStatus}"
+  '';
 in
 lib.mkMerge [
   {
@@ -31,6 +67,7 @@ lib.mkMerge [
         natural_scroll enabled
       }
     '';
+    environment.etc."sway/config".source = lib.mkForce swayConfig;
 
     services.greetd = {
       enable = true;
@@ -54,6 +91,14 @@ lib.mkMerge [
     environment.etc."sway/config.d/10-fcitx5.conf".text = ''
       exec ${config.i18n.inputMethod.package}/bin/fcitx5 -d --replace
       exec dbus-update-activation-environment --systemd XMODIFIERS GTK_IM_MODULE QT_IM_MODULE SDL_IM_MODULE
+    '';
+  })
+
+  (lib.mkIf pipewireEnabled {
+    environment.etc."sway/config.d/30-volume.conf".text = ''
+      bindsym --locked $mod+F8 exec ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bindsym --locked $mod+F9 exec ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+      bindsym --locked $mod+F10 exec ${pkgs.wireplumber}/bin/wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+
     '';
   })
 ]
